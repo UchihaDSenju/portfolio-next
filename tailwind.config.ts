1dfddf026b646194062b77e147ffee7768482150
Tariq <rafiqtariq19@gmail.com>
2023-08-07 23:47:59 +0530

    Project created and layout designed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      fontFamily: {
+        normalFont: ["Sigmar", "cursive", "Space Grotesk", "sans-serif"],
+        cursiveFont: [ "Caveat", "cursive", "Sigmar", "cursive", "Space Grotesk", "sans-serif"]
+      }
     },
   },
   plugins: [],
