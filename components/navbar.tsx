48c11575ecc58050ac445bcbadb3c9015c3a031a
Tariq <rafiqtariq19@gmail.com>
2023-08-08 18:11:45 +0530

    Navbar Created

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className="font-normalFont">Navbar</div>
+    <>
+      <nav className="bg-[#A43030] flex justify-center">
+        <ul className="flex justify-between text-white uppercase w-[500px] px-5">
+          <li>Home</li>
+          <li>Projects</li>
+          <li>Experience</li>
+        </ul>
+      </nav>
+    </>
   )
 }
 
